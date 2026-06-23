e1a3bbb801361144c8f9dabf258a00c4adc77ddd
Kai Zhu <kaizhu256@gmail.com>
2026-06-23 14:32:23 -0500

    - jslint - Relax warning on multiline-method-chaining.

--- sqlmath.mjs
+++ sqlmath.mjs
@@ -708,7 +708,7 @@ async function dbCallAsync(baton, argList, mode, db) {
             profileStart = Date.now();
             sql = String(argList[1]);
             // sql-hash - remove comment
-            sql = sql.replace((/(?:^|\s+?)--.*/gm), "");
+            sql = sql.replace((/\s*?--.*?$/gm), "");
             // sql-hash - remove vowel
             sql = sql.replace((/[aeiou]\b/gi), "\u0000$&");
             sql = sql.replace((/([bcdfghjklmnpqrstvwxyz])[aeiou]+/gi), "$1");
